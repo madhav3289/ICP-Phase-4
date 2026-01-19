91bceb3300be4102861cc1f06f6c78a0c78e5568
madhav3289 <madhavguptadavfps1234@gmail.com>
2026-01-19 16:01:38 +0530

    "Help Classmate Problem Solution added"

---
+++ WEEK_2/Help_Classmates.java
@@ -0,0 +1,31 @@
+package WEEK_2;
+
+import java.util.*;
+
+public class Help_Classmates {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int n=in.nextInt();
+        int [] nums=new int[n];
+        for(int i=0;i<n;i++){
+            nums[i]=in.nextInt();
+        }
+        int [] nse=nextSmallerE(nums, n);
+        for(int i=0;i<n;i++){
+            System.out.print(nse[i]+" ");
+        }
+        in.close();
+    }
+    public static int[] nextSmallerE(int nums[], int n) {
+        int [] nse=new int[n];
+        Stack<Integer> st=new Stack<>();
+        for(int i=n-1;i>=0;i--){
+            while(!st.isEmpty() && nums[i]<=st.peek()){
+                st.pop();
+            }
+            nse[i]=(!st.isEmpty()?st.peek():-1);
+            st.push(nums[i]);
+        }
+        return nse;
+    }
+}
---
+++ WEEK_2/Valid_Parenthesis_String.java
@@ -0,0 +1,15 @@
+package WEEK_2;
+
+import java.util.*;
+
+public class Valid_Parenthesis_String {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        String s=in.next();
+        System.out.println(checkValidString(s));
+        in.close();
+    }
+    public static boolean checkValidString(String s) {
+        return true;
+    }
+}
