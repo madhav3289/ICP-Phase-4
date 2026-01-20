a6cb3e332d35a393c5a9481979899fb69dde4d82
madhav3289 <madhavguptadavfps1234@gmail.com>
2026-01-20 12:46:02 +0530

    "Added Reverse Using Stack problem solution"

---
+++ WEEK_2/Reverse_Using_Stack.java
@@ -0,0 +1,24 @@
+package WEEK_2;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Reverse_Using_Stack {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        String s=in.next();
+        System.out.println(reverse(s));
+        in.close();
+    }
+    public static String reverse(String S) {
+        Stack<Character> st=new Stack<>();
+        for(int i=0;i<S.length();i++){
+            st.push(S.charAt(i));
+        }
+        StringBuilder sb=new StringBuilder();
+        while(!st.isEmpty()){
+            sb.append(st.pop());
+        }
+        return sb.toString();
+    }
+}
